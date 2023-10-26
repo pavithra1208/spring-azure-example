8eb433cda1798fc3bda672b73821e0b2ac65a25b
pavithra.ks <pavithra.ks@ltts.com>
2023-10-26 13:20:12 +0530

    third commit

--- src/main/java/com/example/demo/SpringAzureExampleApplication.java
+++ src/main/java/com/example/demo/SpringAzureExampleApplication.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class SpringAzureExampleApplication {
 	@GetMapping("/message")
 	public String message(){
-		return "Congrats ! your application deployed successfully in Azure Platform. !";
+		return " Application is deployed successfully in Azure Platform. !";
 	}
 
 
